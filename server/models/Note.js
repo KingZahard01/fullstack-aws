8a1eb0956eaf54f1a4b83b967c93d8905c76e76b
juan garcia <juanjose10312@gmail.com>
2024-12-07 17:44:11 -0700

    Modelo de datos para las notas con Sequelize

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,1 +1,25 @@
 // Modelo de datos para las notas
+
+// importamos DataTypes de Sequelize para definir los tipos de datos de las columnas de la tabla
+const { DataTypes } = require("sequelize");
+// Importamos la conexion a la base de datos
+const sequelize = require("../db/db.js");
+
+// Creamos un modelo de datos para las notas
+const Note = sequelize.define("Note", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+// Exportamos el modelo de datos para usarlo en otros archivos
+module.exports = Note;
