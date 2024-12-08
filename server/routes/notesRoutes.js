26a759ade704de53dd56b6aeee135a0b909eef1b
juan garcia <juanjose10312@gmail.com>
2024-12-07 19:42:38 -0700

    Configura las rutas

--- server/routes/notesRoutes.js
+++ server/routes/notesRoutes.js
@@ -1,1 +1,24 @@
 // Rutas relacionadas con las notas
+
+// Importamos Express y el enrutador
+const express = require("express");
+const router = express.Router();
+
+// Importamos las funciones del controlador
+const {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} = require("../controllers/notesController.js");
+
+// Definimos las rutas relacionadas con las notas
+router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+// Exportamos el enrutador
+module.exports = router;
